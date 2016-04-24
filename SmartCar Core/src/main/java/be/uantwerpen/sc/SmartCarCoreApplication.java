b7da41ea968bc5183438ab4228cbe19960fc3f22
Thomas Huybrechts <huybthomas@gmail.com>
2016-04-24 13:34:33 +0200

    Initial commit

---
+++ SmartCar Core/src/main/java/be/uantwerpen/sc/SmartCarCoreApplication.java
@@ -0,0 +1,12 @@
+package be.uantwerpen.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCarCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCarCoreApplication.class, args);
+	}
+}
