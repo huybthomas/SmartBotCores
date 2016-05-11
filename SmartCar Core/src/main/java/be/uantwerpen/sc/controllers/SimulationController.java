6d35a2f13e5fc14b08718ccc06c3785e71526c40
Slein04 <nielsvervliet04@hotmail.com>
2016-05-12 01:16:28 +0200

    remote simulator mode added

--- SmartCar Core/src/main/java/be/uantwerpen/sc/controllers/SimulationController.java
+++ SmartCar Core/src/main/java/be/uantwerpen/sc/controllers/SimulationController.java
@@ -1,6 +1,7 @@
 package be.uantwerpen.sc.controllers;
 
 import be.uantwerpen.sc.services.QueueService;
+import be.uantwerpen.sc.services.SimulationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,8 @@ public class SimulationController {
 
     @Autowired
     private QueueService queueService;
+    @Autowired
+    private SimulationService simulationService;
 
     @RequestMapping(value = "/command/", method = RequestMethod.POST)
     public @ResponseBody ResponseEntity<String> commandListener(@RequestBody String command, UriComponentsBuilder ucBuilder){
@@ -26,5 +29,12 @@ public class SimulationController {
             queueService.insertJob(command);
             return new ResponseEntity("ok",HttpStatus.OK);
         }
+    }
+
+    @RequestMapping(value = "/simulate/", method = RequestMethod.POST)
+    public @ResponseBody ResponseEntity<String> commandListener(@RequestBody boolean simulate, UriComponentsBuilder ucBuilder){
+        simulationService.setActiveSimulator(simulate);
+        System.out.println("Robot simulate mode = " + simulate);
+        return new ResponseEntity("ok",HttpStatus.OK);
     }
 }
