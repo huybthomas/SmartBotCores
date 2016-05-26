c3d27654d9dca9fe89323dfdae7a459e6d313419
Thomas Huybrechts <huybthomas@gmail.com>
2016-05-26 20:18:11 +0200

    Removed unused imports

--- SmartCar Core/src/main/java/be/uantwerpen/sc/controllers/JobListenerController.java
+++ SmartCar Core/src/main/java/be/uantwerpen/sc/controllers/JobListenerController.java
@@ -6,8 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.http.HttpHeaders;
-import sun.invoke.empty.Empty;
 
 import java.util.concurrent.BlockingQueue;
 
